c8c9ae48d9262bc1f900b8c16e330d4211d57e29
kasper henningsen <sejekasper@gmail.com>
2021-04-07 13:35:15 +0200

    added everything

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class ="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="https://i1.sndcdn.com/artworks-000519982329-1xy8d3-t500x500.jpg" alt="">
+        </div>
+        <div class="col-9">
+        
+        </div>
+    </div>
+</div>
+@endsection
